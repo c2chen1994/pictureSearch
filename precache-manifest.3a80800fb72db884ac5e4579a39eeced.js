1f29b812a1e26cd9175641ff31b45b799e4a3464
Zhuo Chen <chen519@usc.edu>
2019-03-11 19:25:05 -0700

    Updates

---
+++ precache-manifest.3a80800fb72db884ac5e4579a39eeced.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddfad90c6eb6f654a1ac",
+    "url": "/pictureSearch/static/css/main.dc76d769.chunk.css"
+  },
+  {
+    "revision": "ddfad90c6eb6f654a1ac",
+    "url": "/pictureSearch/static/js/main.ddfad90c.chunk.js"
+  },
+  {
+    "revision": "3bb143eae8ef4d89f999",
+    "url": "/pictureSearch/static/js/runtime~main.3bb143ea.js"
+  },
+  {
+    "revision": "cb016095e848669d37ac",
+    "url": "/pictureSearch/static/js/2.cb016095.chunk.js"
+  },
+  {
+    "revision": "5c0bb48bfaabc9d7b629c742dad26f3f",
+    "url": "/pictureSearch/index.html"
+  }
+];
